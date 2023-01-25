3fa46a85254e159a2fb230fd7da6f7142376c6ac
Ashiqsubair <ashiksubair1@gmail.com>
2023-01-25 16:01:43 +0530

    Commit

---
+++ Slot11_2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Slot11_2 {
+    public static void main(String args[]) {
+        int a[]={1,2,3,4,5,6};
+        float b[]={2.5F,3.3F,4.2F,5.6F,6.4F,7.3F};
+        String c[]={"Ashiq","Ashin","Abhay","Aromal","Arjun","Gokul"};
+    }
+    public static <Generic> void printa(Generic[] a){
+        for()
+    }
+}
