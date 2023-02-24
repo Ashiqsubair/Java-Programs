75c26b89285515e387c46812e57db231e1fef938
ASHIQ S <72742730+Ashiqsubair@users.noreply.github.com>
2023-02-24 14:48:27 +0530

    Add files via upload

---
+++ Slot11_2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+class Slot11_2 {
+    public static void main(String args[]) {
+        int[] a={1,2};
+        print(a);
+        // in.prin(b);
+        // in.prin(c);
+
+    }
+    static <Gen> void prin(Gen[] a){
+        for(int i=0;i<2;i++){
+            System.out.println(a[i]);
+        }
+        
+    }
+    
+}
+
+    
+    
